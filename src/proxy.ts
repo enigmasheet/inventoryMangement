5f2096cccb226cd1875ea250ee96af0035701243
Abhay Mandal <abhaymandal321@gmail.com>
2026-06-22 01:21:26 +0545

    refactor: remove authentication and logging from proxy function

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,21 +1,10 @@
-import { auth } from "@/lib/auth";
 import { NextResponse } from "next/server";
-import { createLogger } from "@/lib/logger";
-
-const log = createLogger("proxy");
 
 const STATIC_SKIP = ["/_next", "/api/", "/sw.js", "/manifest.json", "/icons/"];
 
 export default async function proxy(request: Request) {
   const url = new URL(request.url);
   if (STATIC_SKIP.some((p) => url.pathname.startsWith(p))) return NextResponse.next();
-
-  const session = await auth.api.getSession({ headers: request.headers });
-  if (!session) {
-    log.info("redirecting unauthenticated request", { path: url.pathname });
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-  log.debug("authenticated request", { path: url.pathname });
   return NextResponse.next();
 }
 
